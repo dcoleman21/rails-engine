a4f0ad84912aec5dcf1bbf740bd4e25d2bb10607
Dani Coleman <danicoleman00@gmail.com>
2020-12-12 12:46:33 -0700

    Add tests for getting all items and for getting a single item by id

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -7,21 +7,68 @@ describe "Items API" do
     get '/api/v1/items'
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
+    items = json[:data]
 
     expect(items.count).to eq(4)
 
     items.each do |item|
       expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(Integer)
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_a(String)
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a(String)
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a(Float)
-      expect(item).to have_key(:merchant_id)
-      expect(item[:merchant_id]).to be_an(Integer)
+      expect(item[:id]).to be_an(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq("item")
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+
+      item_data = item[:attributes]
+
+      expect(item_data).to have_key(:name)
+      expect(item_data[:name]).to be_a(String)
+
+      expect(item_data).to have_key(:description)
+      expect(item_data[:description]).to be_a(String)
+
+      expect(item_data).to have_key(:unit_price)
+      expect(item_data[:unit_price]).to be_a(Float)
+
+      expect(item_data).to have_key(:merchant_id)
+      expect(item_data[:merchant_id]).to be_an(Integer)
     end
+  end
+
+  it "can get a single item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    item = json[:data]
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to eq("item")
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    item_data = item[:attributes]
+
+    expect(item_data).to have_key(:name)
+    expect(item_data[:name]).to be_a(String)
+
+    expect(item_data).to have_key(:description)
+    expect(item_data[:description]).to be_a(String)
+
+    expect(item_data).to have_key(:unit_price)
+    expect(item_data[:unit_price]).to be_a(Float)
+
+    expect(item_data).to have_key(:merchant_id)
+    expect(item_data[:merchant_id]).to be_an(Integer)
   end
 end
