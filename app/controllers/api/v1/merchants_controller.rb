5be355c776ba7130907873c326027a7c149c8549
Dani Coleman <danicoleman00@gmail.com>
2020-12-13 11:13:16 -0700

    Add create, update, destroy and private merchant params to the merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,22 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def create
+    render json: MerchantSerializer.new(Merchant.create(merchant_params))
+  end
+
+  def update
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
+  end
+
+  def destroy
+    Merchant.delete(params[:id])
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
