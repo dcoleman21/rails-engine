5ff140c300ef8d7ad53a165226cf6018f3d6d6d3
Dani Coleman <danicoleman00@gmail.com>
2020-12-13 10:30:44 -0700

    Add test to get a single merchant by id endpoint

--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -30,6 +30,27 @@ describe "Merchants API" do
   end
 
   it "can get a single merchant by id" do
-    
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    merchant = json[:data]
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to eq("merchant")
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes]).to be_a(Hash)
+
+    merchant_attr = merchant[:attributes]
+
+    expect(merchant_attr).to have_key(:name)
+    expect(merchant_attr[:name]).to be_a(String)
   end
 end
