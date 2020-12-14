89074615d7f06328f0a2fff2c0f702ee8eb0b3b5
Dani Coleman <danicoleman00@gmail.com>
2020-12-14 16:05:27 -0700

    Start test for business intelligence endpoints - can get merchant with most revenue

---
+++ spec/requests/api/v1/merchants/business_intel_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Business Intelligence Endpoints" do
+  describe "Revenue based on successful transaction and shipped to the customer " do
+    before :each do
+      create_list(:merchant, 5)
+    end
+    it "can get merchants with most revenue" do
+
+    end
+  end
+end
