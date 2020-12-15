f841e2ea9a8b68a34485fd9f6bedc28c4119bd92
Dani Coleman <danicoleman00@gmail.com>
2020-12-15 15:51:04 -0700

    Add class method for most_sold_items using ActiveRecord to get merchant with most sold items

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -7,4 +7,13 @@ class MerchantFacade
      .order("most_revenue DESC")
      .limit(quantity)
   end
+
+  def self.most_sold_items(quantity)
+    Merchant.joins(invoices: [:transactions, :invoice_items])
+     .select("merchants.*, sum(invoice_items.quantity) as most_sold_items")
+     .where("invoices.status='shipped' AND transactions.result='success'")
+     .group("merchants.id")
+     .order("most_sold_items DESC")
+     .limit(quantity)
+  end
 end
