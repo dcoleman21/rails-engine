639c5622bfa30c413069421cd82fb7a53117a73a
Dani Coleman <danicoleman00@gmail.com>
2020-12-15 16:41:02 -0700

    Add rev_by_dates action to merchants business controller

--- app/controllers/api/v1/merchants/business_controller.rb
+++ app/controllers/api/v1/merchants/business_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::Merchants::BusinessController < ApplicationController
     merchants = MerchantFacade.most_sold_items(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
+
+  def rev_by_dates
+    total_revenue = RevenueFacade.rev_by_dates(date_params)
+    render json: RevenueSerializer.new(total_revenue)
+  end
+
+  private
+
+  def date_params
+    params.permit(:start, :end)
+  end
 end
