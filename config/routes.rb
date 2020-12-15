febd65e44cc52047e935b7775b0a0ef5e3e37286
Dani Coleman <danicoleman00@gmail.com>
2020-12-15 15:48:47 -0700

    Add route for most_items to business controller most_items action

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/:id/items', to: 'items#index'
         get '/most_revenue', to: 'business#most_revenue'
+        get '/most_items', to: 'business#most_items'
       end
       resources :merchants
     end
