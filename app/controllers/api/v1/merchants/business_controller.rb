87476bc9811362ee22d33a5d7fbfc4465db16d3a
Dani Coleman <danicoleman00@gmail.com>
2020-12-15 15:50:21 -0700

    Add most_items action that calls merchant facade active record for most sold items and renders json created in the merchant serializer

--- app/controllers/api/v1/merchants/business_controller.rb
+++ app/controllers/api/v1/merchants/business_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Merchants::BusinessController < ApplicationController
     merchants = MerchantFacade.most_revenue(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
+
+  def most_items
+    merchants = MerchantFacade.most_sold_items(params[:quantity])
+    render json: MerchantSerializer.new(merchants)
+  end
 end
