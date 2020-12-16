efeb3b9a7245d6b73967a95323f3289747bf58bc
Dani Coleman <danicoleman00@gmail.com>
2020-12-15 17:56:23 -0700

    Deleted merchant facade. Now calling merchant model

--- app/controllers/api/v1/merchants/business_controller.rb
+++ app/controllers/api/v1/merchants/business_controller.rb
@@ -1,17 +1,21 @@
 class Api::V1::Merchants::BusinessController < ApplicationController
   def most_revenue
-    merchants = MerchantFacade.most_revenue(params[:quantity])
+    merchants = Merchant.most_revenue(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
 
   def most_items
-    merchants = MerchantFacade.most_sold_items(params[:quantity])
+    merchants = Merchant.most_sold_items(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
 
   def rev_by_dates
     total_revenue = RevenueFacade.rev_by_dates(date_params)
     render json: RevenueSerializer.new(total_revenue)
+  end
+
+  def total_revenue
+    render json: RevenueSerializer.new(Merchant.total_revenue(params[:id]))
   end
 
   private
