6031e49ff5c43e640daf911c56ab66645ab79e53
Dani Coleman <danicoleman00@gmail.com>
2020-12-15 17:55:58 -0700

    Add most revenue, most items and total revenue to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,28 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates :name, presence: true
+
+  def self.most_revenue(quantity)
+    joins(invoices: [:transactions, :invoice_items])
+     .select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as most_revenue")
+     .where("invoices.status='shipped' AND transactions.result='success'")
+     .group("merchants.id")
+     .order("most_revenue DESC")
+     .limit(quantity)
+  end
+
+  def self.most_sold_items(quantity)
+    joins(invoices: [:transactions, :invoice_items])
+     .select("merchants.*, sum(invoice_items.quantity) as most_sold_items")
+     .where("invoices.status='shipped' AND transactions.result='success'")
+     .group("merchants.id")
+     .order("most_sold_items DESC")
+     .limit(quantity)
+  end
+
+  # def self.total_revenue(id)
+  #   joins(invoices: [:invoice_items, :transactions])
+  #     .where("invoices.status='shipped' AND transactions.result='success' AND #{id: id}")
+  #     .sum("invoice_items.quantity * invoice_items.unit_price")
+  # end
 end
