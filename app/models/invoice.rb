9f6fc19eae871fc6c138f9f7ad842ebcf0d4bc36
Dani Coleman <danicoleman00@gmail.com>
2020-12-16 11:32:42 -0700

    Add scope for status shipped to equal successful

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,6 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  scope :successful, -> { where(status: "shipped") }
 end
