a214e74bbd3664c7c15d13a7210e8136b1efc175
Dani Coleman <danicoleman00@gmail.com>
2020-12-16 11:33:17 -0700

    Refactor ActiveRecord method for revenue by dates to follow what we learned in class

--- app/facades/revenue_facade.rb
+++ app/facades/revenue_facade.rb
@@ -2,6 +2,8 @@ class RevenueFacade
   def self.rev_by_dates(params)
     Invoice.joins(:transactions, :invoice_items)
       .select("sum(invoice_items.quantity * invoice_items.unit_price) as total")
-      .where("invoices.status='shipped' AND transactions.result='success' AND invoices.created_at between '#{params['start']} 00:00:00' and '#{params['end']} 23:59:59'")[0]
+      .merge(Transaction.unscoped.successful)
+      .merge(Invoice.unscoped.successful)
+      .where("invoices.created_at between '#{params['start']} 00:00:00' and '#{params['end']} 23:59:59'")[0]
   end
 end
