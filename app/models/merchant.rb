61a90f435cb99ec5a9556f7439c4b964dc53412d
Dani Coleman <danicoleman00@gmail.com>
2020-12-16 14:10:09 -0700

    Finish ActiveRecord logic to get revenue for a single merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -18,8 +18,8 @@ class Merchant < ApplicationRecord
   def self.most_revenue(quantity)
     select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
       .joins(invoices: [:invoice_items, :transactions])
-      .merge(Transaction.unscoped.successful)
-      .merge(Invoice.unscoped.successful)
+      .merge(Transaction.successful)
+      .merge(Invoice.successful)
       .group(:id)
       .order('revenue DESC')
       .limit(quantity)
@@ -28,25 +28,19 @@ class Merchant < ApplicationRecord
   def self.most_sold_items(quantity)
     select("merchants.*, SUM(invoice_items.quantity) AS quantity_of_items")
       .joins(invoices: [:invoice_items, :transactions])
-      .merge(Transaction.unscoped.successful)
-      .merge(Invoice.unscoped.successful)
+      .merge(Transaction.successful)
+      .merge(Invoice.successful)
       .group(:id)
       .order('quantity_of_items DESC')
       .limit(quantity)
   end
 
-  def self.total_revenue(_id)
-    select("merchants.id")
+  def self.total_revenue(id)
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total")
       .joins(invoices: %i[invoice_items transactions])
       .merge(Invoice.successful)
       .merge(Transaction.successful)
-      .sum('invoice_items.quantity * invoice_items.unit_price')
+      .where("merchant_id = #{id}")
+      .group(:id).first
   end
-  # Failure/Error: object.total
-  #
-  #    NoMethodError:
-  #      undefined method `total' for 2520.0:Float
-  #    # ./app/serializers/revenue_serializer.rb:5:in `block in <class:RevenueSerializer>'
-  #    # ./app/controllers/api/v1/merchants/business_controller.rb:18:in `merchant_revenue'
-  #    # ./spec/requests/api/v1/merchants/business_intel_spec.rb:126:in `block (3 levels) in <top (required)>'
 end
